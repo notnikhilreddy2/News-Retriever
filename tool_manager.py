2463d1bb3aa4872791f6c00209836a4a37f9b03e
Nikhil Reddy <nikhilreddy.maligireddy2@gmail.com>
2025-05-02 11:47:55 -0400

    changed news source

--- tool_manager.py
+++ tool_manager.py
@@ -10,51 +10,53 @@ from typing import Annotated
 from gnews import GNews
 
 
+
 class ToolManager:
     def __init__(self, article_count=10, cache_dir=".cache", exclude_websites=None):
         self.article_count = article_count
         self.cache_dir = cache_dir
         self.urls_file = f"{self.cache_dir}/news_database.csv"
         self.google_news = GNews()
-        self.google_news.period = '1h'
+        self.google_news.period = '24h'
         self.google_news.max_results = self.article_count
         self.google_news.country = 'United States'
         self.google_news.language = 'english'
         self.google_news.exclude_websites = exclude_websites if exclude_websites else ['cnn.com']
         self.shortener = Shortener(timeout=5)
+        self.NEWSAPI_KEY = os.getenv("NEWSAPI_KEY", 'bdb6ca050c0742b3a0e2dba3f593a338')
 
         if not os.path.exists(self.cache_dir):
             os.makedirs(self.cache_dir)
 
-    def get_decoding_params(self, gn_art_id):
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        response = requests.get(f"https://news.google.com/articles/{gn_art_id}", headers=headers)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, "lxml")
-        div = soup.select_one("c-wiz > div")
-        return {
-            "signature": div.get("data-n-a-sg"),
-            "timestamp": div.get("data-n-a-ts"),
-            "gn_art_id": gn_art_id,
-        }
+    # def get_decoding_params(self, gn_art_id):
+    #     headers = {'User-Agent': 'Mozilla/5.0'}
+    #     response = requests.get(f"https://news.google.com/articles/{gn_art_id}", headers=headers)
+    #     response.raise_for_status()
+    #     soup = BeautifulSoup(response.text, "lxml")
+    #     div = soup.select_one("c-wiz > div")
+    #     return {
+    #         "signature": div.get("data-n-a-sg"),
+    #         "timestamp": div.get("data-n-a-ts"),
+    #         "gn_art_id": gn_art_id,
+    #     }
 
-    def decode_urls(self, articles):
-        articles_reqs = [
-            [
-                "Fbv4je",
-                f'["garturlreq",[["X","X",["X","X"],null,null,1,1,"US:en",null,1,null,null,null,null,null,0,1],"X","X",1,[1,1,1],1,1,null,0,0,null,0],"{art["gn_art_id"]}",{art["timestamp"]},"{art["signature"]}"]',
-            ]
-            for art in articles
-        ]
-        payload = f"f.req={quote(json.dumps([articles_reqs]))}"
-        headers = {"content-type": "application/x-www-form-urlencoded;charset=UTF-8"}
-        response = requests.post(
-            url="https://news.google.com/_/DotsSplashUi/data/batchexecute",
-            headers=headers,
-            data=payload,
-        )
-        response.raise_for_status()
-        return [json.loads(res[2])[1] for res in json.loads(response.text.split("\n\n")[1])[:-2]]
+    # def decode_urls(self, articles):
+    #     articles_reqs = [
+    #         [
+    #             "Fbv4je",
+    #             f'["garturlreq",[["X","X",["X","X"],null,null,1,1,"US:en",null,1,null,null,null,null,null,0,1],"X","X",1,[1,1,1],1,1,null,0,0,null,0],"{art["gn_art_id"]}",{art["timestamp"]},"{art["signature"]}"]',
+    #         ]
+    #         for art in articles
+    #     ]
+    #     payload = f"f.req={quote(json.dumps([articles_reqs]))}"
+    #     headers = {"content-type": "application/x-www-form-urlencoded;charset=UTF-8"}
+    #     response = requests.post(
+    #         url="https://news.google.com/_/DotsSplashUi/data/batchexecute",
+    #         headers=headers,
+    #         data=payload,
+    #     )
+    #     response.raise_for_status()
+    #     return [json.loads(res[2])[1] for res in json.loads(response.text.split("\n\n")[1])[:-2]]
     # from googlenewsdecoder import gnewsdecoder
 
      #def decode_urls(self, articles):
@@ -132,22 +134,31 @@ class ToolManager:
 
         for keyword in keyword_list:
             try:
-                sources = self.google_news.get_news(keyword)
+                # sources = self.google_news.get_news(keyword)
+                url = ('https://newsapi.org/v2/top-headlines?'
+                    'country=us&'
+                    f'apiKey={self.NEWSAPI_KEY}')
+                # print('URL: ', url)
+                response = requests.get(url).json()
+                sources = response['articles']
+                sources = [source['url'] for source in sources if source['url']]
+                # print('SOURCES: ', sources)
+                # print(response.json())
             except Exception as e:
                 print(f"Error fetching news for {keyword}: {e}")
                 continue
-            print('SOURCES: ', sources)
+            # print('SOURCES: ', sources)
             for source in sources:
                 try:
-                    art_id = urlparse(source['url']).path.split("/")[-1]
+                    # art_id = urlparse(source['url']).path.split("/")[-1]
                     # print('Art ID: ', art_id)
-                    params = self.get_decoding_params(art_id)
+                    # params = self.get_decoding_params(art_id)
                     # print('PARAMS: ', params)
-                    decoded = self.decode_urls([params])
+                    # decoded = self.decode_urls([params])
                     # print('DECODED: ', decoded)
-                    if decoded:
-                        urls.extend(decoded)
-                        keywords.extend([keyword])
+                    if source:
+                        urls.append(source)
+                        keywords.append([keyword])
                 except Exception as e:
                     #print stack trace
                     import traceback
@@ -160,9 +171,9 @@ class ToolManager:
             return {"NEWS 1": {"TOPIC": "AI", "TITLE": "Decagon Named to 2025 Forbes AI 50 List of Top Artificial Intelligence Companies", "CONTENT": "SAN FRANCISCO--(BUSINESS WIRE)--Decagon, the leading innovator in conversational AI agents for customer experience, today announced it has been named to the 2025 Forbes AI 50 — Forbes\" annual list of the most promising, privately-held companies using artificial intelligence to shape the future of business and society. This marks Decagon’s first appearance on the prestigious list, which spotlights standout AI companies across North America.\n'Being able to effectively handle tasks like refunds or account changes is table stakes. What sets the best products apart is their ability to meet customers where they are, completing complex workflows and delivering deeply personalized experiences.'\nShare\nWith growing hype around AI agents, few companies have delivered meaningful, enterprise-ready results. Decagon sets itself apart by building AI agents that drive immediate and measurable impact. Its technology is used by well-known companies — including Hertz, Eventbrite, Duolingo, ClassPass, Noti", "SOURCE": "https://tinyurl.com/2dm2xvn4"}}
             # return None
 
-        # print('URLs: ', urls)
+        # print('URLs: ', urls[0])
         article_list = self.read_news_articles(urls, keywords)
-        # print('Article List: ', article_list)
+        # print('Article List: ', len(article_list))
 
         for article in article_list:
             try:
